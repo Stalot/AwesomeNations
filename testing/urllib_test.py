656f023a61eda1732dfc4d071f45a65cd773255e
Orly Antonio Carvalho Neto <orlyprogramador2009@gmail.com>
2025-02-19 23:51:38 -0300

    urllib tests

---
+++ testing/urllib_test.py
@@ -0,0 +1,55 @@
+from urllib import request, parse
+from awesomeNations.exceptions import HTTPError
+from dotenv import load_dotenv
+import os
+from pprint import pprint as pp
+import time
+
+load_dotenv(".env")
+
+calamity_password = os.environ["THE_HOSTS_OF_CALAMITY_PASSWORD"]
+
+class Connection:
+    def __init__(self, headers):
+        self.base_api_url: str = "https://www.nationstates.net/cgi-bin/api.cgi"
+        self.headers: dict = headers
+
+    def urlib_request_url(self, url_params: dict[str, str]):
+        querystring = parse.urlencode(url_params)
+        url = self.base_api_url + "?" + querystring
+        
+        request_object = request.Request(url, None, headers=self.headers)
+        response = request.urlopen(request_object)
+        
+        self.headers.update({"X-autologin": response.headers.get("X-autologin", ""),
+                            "X-pin": response.headers.get("X-pin", "")})
+        
+        print(self.headers["X-autologin"], self.headers["X-pin"])
+        
+        if response.code != 200:
+            raise HTTPError(response.code)
+        decoded_response = response.read().decode("UTF-8")
+        return decoded_response
+
+class Nation:
+    def __init__(self,
+                 nation_name: str = None,
+                 password: str = None,
+                 autologin: str = None):
+        self.nation_name = nation_name
+        self.password = password
+        self.autologin = autologin
+        
+        headers = {"User-Agent": "Urllib connection test (by: Orlys)",
+                   "X-Password": password if password else "",
+                   "X-Autologin": autologin if autologin else ""}
+        self.connection = Connection(headers)
+        
+    def get_data(self, shards: list[str] | tuple[str]) -> None:
+        shards = "+".join(shards)
+        params = {"nation": self.nation_name,
+                  "q": shards}
+        print(self.connection.urlib_request_url(params))
+
+nation = Nation("testlandia")
+nation.get_data(["name", "fullname", "motto"])
